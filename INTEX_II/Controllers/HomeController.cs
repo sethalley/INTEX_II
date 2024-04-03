afa5f11275c333f6241d9c106cac858d493736bd
Seth Alley <salley32@byu.edu>
2024-04-03 14:47:23 -0600

    Add project files.

---
+++ INTEX_II/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using INTEX_II.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace INTEX_II.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult ONNX()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ INTEX_II/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INTEX_II.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
